3fe4355c2154066b0d90c11be8a3e5c10f13f8bf
Lars de Kooter <lars@larsdekooter.nl>
2024-04-26 08:42:57 +0200

    copy-paste

--- network.py
+++ network.py
@@ -44,29 +44,38 @@ class QTrainer:
         self.criterion = nn.MSELoss()
 
     def trainStep(self, state, action, reward, nextState, done):
-        state = torch.tensor(np.array(state), dtype=torch.float)
-        nextState = torch.tensor(np.array(nextState), dtype=torch.float)
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
         action = torch.tensor(action, dtype=torch.long)
         reward = torch.tensor(reward, dtype=torch.float)
+        # (n, x)
 
         if len(state.shape) == 1:
+            # (1, x)
             state = torch.unsqueeze(state, 0)
-            nextState = torch.unsqueeze(nextState, 0)
+            next_state = torch.unsqueeze(next_state, 0)
             action = torch.unsqueeze(action, 0)
             reward = torch.unsqueeze(reward, 0)
-            done = (done,)
+            done = (done, )
 
+        # 1: predicted Q values with current state
         pred = self.model(state)
 
         target = pred.clone()
         for idx in range(len(done)):
             Q_new = reward[idx]
             if not done[idx]:
-                Q_new = reward[idx] + self.gamma * torch.max(self.model(nextState[idx]))
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+
             target[idx][torch.argmax(action[idx]).item()] = Q_new
+    
+        # 2: Q_new = r + y * max(next_predicted Q value) -> only do this if not done
+        # pred.clone()
+        # preds[argmax(action)] = Q_new
         self.optimizer.zero_grad()
         loss = self.criterion(target, pred)
         loss.backward()
+
         self.optimizer.step()
 
 
