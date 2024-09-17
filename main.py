02f6e03fd5e29e84d498406bd35e60df9391ca0b
Lars de Kooter <de.kooter.lars@gmail.com>
2024-09-17 16:18:00 +0200

    Rewrite

--- main.py
+++ main.py
@@ -1,89 +1,46 @@
 from game import Game
-import pygame
 from network import Network
-import data
-import matplotlib.pyplot as plt
 from tqdm import tqdm
+import data
 
-game = Game()
+game = Game(False)
 network = Network()
-state_old = network.getState(game)
+
+state = network.getState(game)
 
 for i in tqdm(range(4366728)):
-    final_move = [0, 0, 0, 0, 0]
-    move = network.getMove(state_old)
-    final_move[move] = 1
-    reward, done, score = game.step(final_move, render=True)
-    stateNew = network.getState(game)
-    network.train(state_old, stateNew, move, reward, done)
-    state_old = stateNew
+    action = network.getMove(state)
+    finalmove = [0, 0, 0, 0]
+    finalmove[action] = 1
+    reward, done, score = game.step(finalmove)
+    newState = network.getState(game)
+    network.train(state, newState, action, reward, done)
+    state = newState
     if done:
-        # print(
-        #     f"Games: {network.ngames}, Score: {score}, Percentage: {round(network.aiPerGame[network.ngames] / (network.aiPerGame[network.ngames] + network.randomPerGame[network.ngames]) * 100.0, 2)}, Epsilon: {round(network.epsilon, 3)}, Steps: {network.step}"
-        # )
         game.reset()
-        game.ngames += 1
+        state = network.getState(game)
         network.ngames += 1
         network.aiPerGame.append(0)
         network.randomPerGame.append(0)
         if network.epsilon <= data.minEpsilon:
             break
-game.reset()
-game.ngames += 1
-network.ngames += 1
-network.aiPerGame.append(0)
-network.randomPerGame.append(0)
+
+game = Game(True)
+state = network.getState(game)
 while True:
-    final_move = [0, 0, 0, 0, 0]
-    move = network.getMove(state_old)
-    final_move[move] = 1
-    reward, done, score = game.step(final_move, render=True)
-    stateNew = network.getState(game)
-    network.train(state_old, stateNew, move, reward, done)
-    state_old = stateNew
+    action = network.getMove(state)
+    finalmove = [0, 0, 0, 0]
+    finalmove[action] = 1
+    reward, done, score = game.step(finalmove)
+    newState = network.getState(game)
+    network.train(state, newState, action, reward, done)
+    state = newState
     if done:
         print(
-            f"Games: {network.ngames}, Score: {score}, Percentage: {round(network.aiPerGame[network.ngames] / (network.aiPerGame[network.ngames] + network.randomPerGame[network.ngames]) * 100.0, 2)}, Epsilon: {round(network.epsilon, 3)}, Steps: {network.step}"
+            f"Games: {network.ngames}, Score: {score}, Percentage: {round(100.0 * network.aiPerGame[network.ngames] / (network.aiPerGame[network.ngames] + network.randomPerGame[network.ngames]), 2)}%, Epsilon: {network.epsilon}"
         )
         game.reset()
-        game.ngames += 1
+        state = network.getState(game)
         network.ngames += 1
         network.aiPerGame.append(0)
         network.randomPerGame.append(0)
-
-
-def get_moves():
-    keys = pygame.key.get_pressed()
-    final_move = [0, 0, 0, 0]
-    if keys[pygame.K_LEFT]:
-        return 2
-    elif keys[pygame.K_RIGHT]:
-        return 3
-    elif keys[pygame.K_UP]:
-        return 1
-    elif keys[pygame.K_DOWN]:
-        return 0
-    else:
-        return 4
-
-
-def reverseTranslateMoves(move):
-    if move == "B":
-        return 0
-    if move == "F":
-        return 1
-    if move == "L":
-        return 2
-    if move == "R":
-        return 3
-
-
-def translate_moves(move):
-    if move[0] == 1:
-        return "Backwards"
-    elif move[1] == 1:
-        return "Forwards"
-    if move[2] == 1:
-        return "Left"
-    if move[3] == 1:
-        return "Right"
--- network.py
+++ network.py
@@ -1,71 +1,72 @@
-import torch
 import torch.nn as nn
+import torch
 import numpy as np
 import data
 import torch.optim as optim
 from collections import deque
 from game import Game
-import util
+from util import getDistanceToPoint, getDistanceToLine
 import random
 
 
 class DQN(nn.Module):
-    def __init__(self):
+    def __init__(self, inp, out):
         super().__init__()
-        self.stack = nn.Sequential(
-            nn.Linear(data.inputSize, data.hiddenSize),
+        self.layers = nn.Sequential(
+            nn.Linear(inp, 32),
             nn.ReLU(),
-            nn.Linear(data.hiddenSize, data.hiddenSize),
+            nn.Linear(32, 32),
             nn.ReLU(),
-            nn.Linear(data.hiddenSize, data.outputSize),
+            nn.Linear(32, out),
         )
 
     def forward(self, x):
-        return self.stack(x)
+        return self.layers(x)
 
 
 class Network:
     def __init__(self):
-        self.model = DQN()
+        self.model = DQN(13, 4)
+        self.targetModel = DQN(13, 4)
+        self.updateTargetModel()
         self.optimizer = optim.Adam(self.model.parameters(), lr=data.lr)
         self.criterion = nn.MSELoss()
-        self.memory = deque(maxlen=100_000)
+        self.memory = deque(maxlen=10000)
         self.step = 0
         self.randomPerGame = [0]
         self.aiPerGame = [0]
         self.ngames = 0
-        self.targetModel = DQN()
-        self.targetModel.load_state_dict(self.model.state_dict())
 
     def getState(self, game: Game):
         distanceToWalls = []
-        for raycast in game.car.raycastlines:
-            points = raycast.get_collision_points(game.circuitLines)
+        for raycast in game.car.raycasts:
+            intersections = raycast.point(game.circuit)
             distances = []
-            for point in points:
-                distances.append(util.getDistanceToPoint(game.car.x, game.car.y, point))
+            for point in intersections:
+                distances.append(
+                    getDistanceToPoint(game.car.x, game.car.y, point[0], point[1])
+                )
             distanceToWalls.append(distances)
 
         currentLine = game.pointLines[game.car.currentLine]
-        distance, xDistance, yDistance = util.getShortestDistanceToLine(
-            game.car.x, game.car.y, currentLine
-        )
-        game.car.lastDistance = distance
+        distance = getDistanceToLine(game.car.x, game.car.y, currentLine)
+
         state = [
-            np.min(distanceToWalls[0] if len(distanceToWalls[0]) > 0 else 1000),
-            np.min(distanceToWalls[1] if len(distanceToWalls[1]) > 0 else 1000),
-            np.min(distanceToWalls[2] if len(distanceToWalls[2]) > 0 else 1000),
-            np.min(distanceToWalls[3] if len(distanceToWalls[3]) > 0 else 1000),
-            np.min(distanceToWalls[4] if len(distanceToWalls[4]) > 0 else 1000),
-            np.min(distanceToWalls[5] if len(distanceToWalls[5]) > 0 else 1000),
-            np.min(distanceToWalls[6] if len(distanceToWalls[6]) > 0 else 1000),
-            np.min(distanceToWalls[7] if len(distanceToWalls[7]) > 0 else 1000),
+            np.min(distanceToWalls[0]) if len(distanceToWalls[0]) > 0 else 1000,
+            np.min(distanceToWalls[1]) if len(distanceToWalls[1]) > 0 else 1000,
+            np.min(distanceToWalls[2]) if len(distanceToWalls[2]) > 0 else 1000,
+            np.min(distanceToWalls[3]) if len(distanceToWalls[3]) > 0 else 1000,
+            np.min(distanceToWalls[4]) if len(distanceToWalls[4]) > 0 else 1000,
+            np.min(distanceToWalls[5]) if len(distanceToWalls[5]) > 0 else 1000,
+            np.min(distanceToWalls[6]) if len(distanceToWalls[6]) > 0 else 1000,
+            np.min(distanceToWalls[7]) if len(distanceToWalls[7]) > 0 else 1000,
             distance,
             game.car.x,
             game.car.y,
             game.car.speed,
             game.car.angle,
         ]
+
         return np.array(state, dtype=np.float32)
 
     def getMove(self, state):
@@ -104,4 +105,7 @@ class Network:
         self.optimizer.step()
 
         if self.step % 100 == 0:
-            self.targetModel.load_state_dict(self.model.state_dict())
+            self.updateTargetModel()
+
+    def updateTargetModel(self):
+        self.targetModel.load_state_dict(self.model.state_dict())
